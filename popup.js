a9fcddb21a62021b69786f7d30ef7d93b07c1016
Derek Gou <134746288+derekGou@users.noreply.github.com>
2023-08-26 16:34:16 -0400

    Update popup.js
    dunno if this gunna work

--- popup.js
+++ popup.js
@@ -1,4 +1,8 @@
 import { main } from "./main.js";
+var emotion = 0;
+var sentiment = 0;
+var mouth1 = document.getElementById("mouth");
+var mouth2 = document.getElementById("othermouth");
 
 document.addEventListener('DOMContentLoaded', function () {
     const uploadBox = document.getElementById('upload-box');
@@ -40,4 +44,53 @@ document.addEventListener('DOMContentLoaded', function () {
 
 async function showTranscript(transcript) {
     console.log("display transcript");
+    if (sentiment_result.text == 'NEUTRAL'){
+        emotion = 0;
+    } else if (sentiment_result.text == 'POSITIVE'){
+        emotion = 1;
+    } else if (sentiment_result.text == 'NEGATIVE'){
+        emotion = -1;
+    }
+    if (emotion != sentiment){
+        if (emotion > sentiment){
+            smile1();
+        } else if (emotion < sentiment){
+            smile2();
+        }
+    }
+    if (emotion==0&&sentiment==0){
+        mouth2.style.display = 'none';
+    } else {
+        mouth2.style.display = 'block';
+    }
+}
+function smile1() {
+    var id1 = null;
+    var rotate = 90+(sentiment*90);
+    clearInterval(id1);
+    id1 = setInterval(move1, 5);
+    function move1() {
+        if (rotate >= 90+(emotion*90)) {
+            clearInterval(id1);
+            sentiment = emotion;
+        } else {
+            rotate+=0.5;
+            mouth1.style.transform = 'rotateY('+rotate+'deg)';
+        }
+    }
+}
+function smile2() {
+    var id2 = null;
+    var rotate = 90+(sentiment*90);
+    clearInterval(id2);
+    id2 = setInterval(move2, 5);
+    function move2() {
+        if (rotate <= 90+(emotion*90)) {
+            clearInterval(id2);
+            sentiment = emotion;
+        } else {
+            rotate-=0.5;
+            mouth2.style.transform = 'rotateY('+rotate+'deg)';
+        }
+    }
 }
