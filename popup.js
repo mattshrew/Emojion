d9e704546ecc84bf91aeecc96b96e2d6dca90811
Derek Gou <134746288+derekGou@users.noreply.github.com>
2023-08-26 22:28:50 -0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -3,6 +3,8 @@ var emotion = 0;
 var sentiment = 0;
 var mouth1 = document.getElementById("mouth");
 var mouth2 = document.getElementById("othermouth");
+var body = document.getElementById("body");
+var content = document.getElementById("content");
 var printer = "";
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -16,6 +18,7 @@ document.addEventListener('DOMContentLoaded', function () {
     fileInput.addEventListener('change', async function () {
         const file = fileInput.files[0];
         if (file) {
+            dropDown();
             console.log('Uploaded file:', file.name);
             const transcript = await main(file);
             await showTranscript(transcript);
@@ -36,6 +39,7 @@ document.addEventListener('DOMContentLoaded', function () {
         uploadBox.classList.remove('dragover');
         const file = e.dataTransfer.files[0];
         if (file) {
+            dropDown();
             console.log('Dropped file:', file.name);
             const transcript = await main(file);
             await showTranscript(transcript);
@@ -43,20 +47,20 @@ document.addEventListener('DOMContentLoaded', function () {
     });
 });
 var tex;
-var senti;
+const sentiArray = [];
 var conf;
-var startt;
-var endd;
+const starttArray = [];
+const enddArray = [];
 const wordArray = [];
 const startArray = [];
 const endArray = [];
 async function showTranscript(transcript) {
     for (let sentiment_result of transcript.sentiment_analysis_results) {
         tex = sentiment_result.text;
-        senti = sentiment_result.sentiment;
         conf = sentiment_result.confidence;
-        startt = sentiment_result.start;
-        endd = sentiment_result.end;
+        starttArray.push(sentiment_result.start);
+        sentiArray.push(sentiment_result.sentiment);
+        enddArray.push(sentiment_result.end);
     }
     for (let word_data of transcript.words) {
         wordArray.push(word_data.text);
@@ -64,24 +68,38 @@ async function showTranscript(transcript) {
         endArray.push(word_data.end);
     }
     runnotate();
-    if (senti == 'NEUTRAL'){
-        emotion = 0;
-    } else if (senti == 'POSITIVE'){
-        emotion = 1;
-    } else if (senti == 'NEGATIVE'){
-        emotion = -1;
-    }
-    if (emotion != sentiment){
-        if (emotion > sentiment){
-            smile1();
-        } else if (emotion < sentiment){
-            smile2();
+    runnotate1();
+}
+function dropDown() {
+    var id = null;
+    var top = 0;
+    clearInterval(id);
+    id = setInterval(dropper, 10);
+    function dropper() {
+        console.log("matthew needs bing chilling");
+        if (top >= 219) {
+            clearInterval(id);
+        } else {
+            top+=5;
+            body.style.height = top+100+'px';
+            content.style.height = top+'px';
         }
     }
-    if (emotion==0&&sentiment==0){
-        mouth2.style.display = 'block';
-    } else {
-        mouth2.style.display = 'none';
+}
+function dropUp() {
+    var id1 = null;
+    var top1 = 220;
+    clearInterval(id1);
+    id1 = setInterval(dropper1, 10);
+    function dropper1() {
+        console.log("matthew needs mor bing chilling");
+        if (top1 <= 0) {
+            clearInterval(id1);
+        } else {
+            top1-=5;
+            body.style.height = top1+100+'px';
+            content.style.height = top1+'px';
+        }
     }
 }
 function smile1() {
@@ -117,7 +135,7 @@ function smile2() {
 var counter = 0;
 var indexWord = 0;
 function runnotate(){
-    if (counter<=endd){
+    if (counter<=enddArray[enddArray.length-1]){
         if (counter>=startArray[indexWord]){
             printer=printer+wordArray[indexWord]+' ';
             indexWord+=1;
@@ -125,5 +143,38 @@ function runnotate(){
         document.getElementById("p1").innerHTML = printer;
         counter+=10;
         setTimeout(runnotate, 10);
+    } else {
+        console.log("bingchilling");
+        setTimeout(dropUp, 5000);
+    }
+}
+var counter1 = 0;
+var indexSent = 0;
+function runnotate1(){
+    if (counter1<=enddArray[enddArray.length-1]){
+        if (counter1>=starttArray[indexSent]){
+            if (sentiArray[indexSent] == 'NEUTRAL'){
+                emotion = 0;
+            } else if (sentiArray[indexSent] == 'POSITIVE'){
+                emotion = 1;
+            } else if (sentiArray[indexSent] == 'NEGATIVE'){
+                emotion = -1;
+            }
+            if (emotion != sentiment){
+                if (emotion > sentiment){
+                    smile1();
+                } else if (emotion < sentiment){
+                    smile2();
+                }
+            }
+            if (emotion==0&&sentiment==0){
+                mouth2.style.display = 'block';
+            } else {
+                mouth2.style.display = 'none';
+            }
+            indexSent+=1;
+        }
+        counter1+=10;
+        setTimeout(runnotate1, 10);
     }
 }
